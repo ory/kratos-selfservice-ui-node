81a943d4cf844846aba23c85ae3b539ae5b1729f
aeneasr <aeneas@ory.sh>
2019-10-24 18:44:36 +0200

    Show headers and decode jwt

--- src/config.ts
+++ src/config.ts
@@ -8,4 +8,5 @@ export default {
     public: (process.env.HIVE_PUBLIC_URL || '').replace(/\/+$/, ''),
   },
   baseUrl: (process.env.BASE_URL || '/').replace(/\/+$/, '') + '/',
+  jwksUrl: process.env.JWKS_URL || '/'
 }
--- src/dashboard.ts
+++ src/dashboard.ts
@@ -2,8 +2,15 @@ import { Request, Response } from 'express'
 import config from './config'
 
 export default (req: Request, res: Response) => {
+  const interestingHeaders = req.rawHeaders.reduce((p: string[], v: string, i) => i % 2 ? p : [...p, `${v}: ${req.rawHeaders[i+1]}`], [])
+
   res.render('dashboard', {
-    identity: {},
+    session: (req as any).user,
+    headers: `
+GET ${req.path} HTTP/1.1
+
+${interestingHeaders.filter((header: string) => /User-Agent|Authorization|Content-Type|Host|Accept-Encoding|Accept-Language|Connection|X-Forwarded-For/.test(header)).join("\n")}
+...`,
     logoutUrl: `${config.hive.public}/auth/browser/logout`,
   })
 }

e3c0d889de405f75237fe84b496551d4ccbae1ec
hackerman <3372410+aeneasr@users.noreply.github.com>
2019-10-24 13:55:59 +0200

    Clean up code and resolve issues (#7)

---
+++ src/auth.ts
@@ -0,0 +1,64 @@
+import { Config } from 'types'
+import { NextFunction, Request, Response } from 'express'
+import config from './config'
+import fetch from 'node-fetch'
+
+// A simple express handler that shows the login / registration screen.
+// Argument "type" can either be "login" or "registration" and will
+// fetch the form data from ORY Hive's Public API.
+export const authHandler = (type: 'login' | 'registration') => (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const request = req.query.request
+
+  // The request is used to identify the login and registraion request and
+  // return data like the csrf_token and so on.
+  if (!request) {
+    console.log('No request found in URL, initializing auth flow.')
+    res.redirect(`${config.hive.browser}/auth/browser/${type}`)
+    return
+  }
+
+  // This is the ORY Hive URL. If this app and ORY Hive are running
+  // on the same (e.g. Kubernetes) cluster, this should be ORY Hive's internal hostname.
+  const url = new URL(`${config.hive.public}/auth/browser/requests/${type}`)
+  url.searchParams.set('request', request)
+
+  fetch(url.toString())
+    .then(response => {
+      if (response.status == 404) {
+        res.redirect(`${config.hive.browser}/auth/browser/${type}`)
+        return
+      }
+
+      return response.json()
+    })
+    .then((request: Config) => {
+      const {
+        methods: {
+          password: {
+            config: {
+              fields: {
+                csrf_token: { value: csrf_token = '' } = {},
+                identifier: { value: identifier = '' } = {},
+                'traits.email': { value: email = '' } = {},
+              } = {},
+              action,
+              errors,
+            },
+          },
+        },
+      } = request
+
+      res.render(type, {
+        formAction: action,
+        csrfToken: csrf_token,
+        identifier: identifier,
+        email: email,
+        errors,
+      })
+    })
+    .catch(err => next(err))
+}
---
+++ src/config.ts
@@ -0,0 +1,11 @@
+export default {
+  hive: {
+    browser: (
+      process.env.HIVE_BROWSER_URL ||
+      process.env.HIVE_PUBLIC_URL ||
+      ''
+    ).replace(/\/+$/, ''),
+    public: (process.env.HIVE_PUBLIC_URL || '').replace(/\/+$/, ''),
+  },
+  baseUrl: (process.env.BASE_URL || '/').replace(/\/+$/, '') + '/',
+}

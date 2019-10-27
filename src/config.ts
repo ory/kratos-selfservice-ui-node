e12decf339588b4cf89c7e58a363e385b79fac1c
aeneasr <aeneas@ory.sh>
2019-10-27 18:49:08 +0100

    Allow customizable project name

--- src/config.ts
+++ src/config.ts
@@ -1,12 +1,13 @@
 export default {
-  hive: {
-    browser: (
-      process.env.HIVE_BROWSER_URL ||
-      process.env.HIVE_PUBLIC_URL ||
-      ''
-    ).replace(/\/+$/, ''),
-    public: (process.env.HIVE_PUBLIC_URL || '').replace(/\/+$/, ''),
-  },
-  baseUrl: (process.env.BASE_URL || '/').replace(/\/+$/, '') + '/',
-  jwksUrl: process.env.JWKS_URL || '/',
+    hive: {
+        browser: (
+            process.env.HIVE_BROWSER_URL ||
+            process.env.HIVE_PUBLIC_URL ||
+            ''
+        ).replace(/\/+$/, ''),
+        public: (process.env.HIVE_PUBLIC_URL || '').replace(/\/+$/, ''),
+    },
+    baseUrl: (process.env.BASE_URL || '/').replace(/\/+$/, '') + '/',
+    jwksUrl: process.env.JWKS_URL || '/',
+    projectName: process.env.PROJECT_NAME || 'SecureApp'
 }
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
-import express, { NextFunction, Request, Response } from 'express'
+import express, {NextFunction, Request, Response} from 'express'
 import handlebars from 'express-handlebars'
-import { authHandler } from './auth'
+import {authHandler} from './auth'
 import errorHandler from './error'
 import dashboard from './dashboard'
 import config from './config'
@@ -8,64 +8,66 @@ import jwks from 'jwks-rsa'
 import jwt from 'express-jwt'
 
 const protect = jwt({
-  // Dynamically provide a signing key based on the kid in the header and the signing keys provided by the JWKS endpoint.
-  secret: jwks.expressJwtSecret({
-    cache: true,
-    jwksRequestsPerMinute: 5,
-    jwksUri: config.jwksUrl,
-  }),
-  algorithms: ['RS256'],
+    // Dynamically provide a signing key based on the kid in the header and the signing keys provided by the JWKS endpoint.
+    secret: jwks.expressJwtSecret({
+        cache: true,
+        jwksRequestsPerMinute: 5,
+        jwksUri: config.jwksUrl,
+    }),
+    algorithms: ['RS256'],
 })
 
 const app = express()
 app.set('view engine', 'hbs')
 
+app.use((req: Request, res: Response, next: NextFunction) => {
+    res.locals.projectName = config.projectName;
+    res.locals.baseUrl =  config.baseUrl;
+    next();
+})
 app.use(express.static('public'))
 app.use(express.static('node_modules/normalize.css'))
 
 app.engine(
-  'hbs',
-  handlebars({
-    extname: 'hbs',
-    layoutsDir: `${__dirname}/../views/layouts/`,
-    partialsDir: `${__dirname}/../views/partials/`,
-    defaultLayout: 'main',
-    helpers: {
-      baseUrl: () => config.baseUrl,
-    },
-  })
+    'hbs',
+    handlebars({
+        extname: 'hbs',
+        layoutsDir: `${__dirname}/../views/layouts/`,
+        partialsDir: `${__dirname}/../views/partials/`,
+        defaultLayout: 'main',
+    })
 )
 
 if (process.env.NODE_ENV === 'only-ui') {
-  // Setting NODE_ENV to "only-ui" disables all integration and only shows the UI. Useful
-  // when working on CSS or HTML things.
-  app.get('/', dashboard)
-  app.get('/auth/registration', (_: Request, res: Response) =>
-    res.render('registration')
-  )
-  app.get('/auth/login', (_: Request, res: Response) => res.render('login'))
-  app.get('/error', (_: Request, res: Response) => res.render('error'))
+    // Setting NODE_ENV to "only-ui" disables all integration and only shows the UI. Useful
+    // when working on CSS or HTML things.
+    app.get('/', dashboard)
+    app.get('/auth/registration', (_: Request, res: Response) =>
+        res.render('registration')
+    )
+    app.get('/auth/login', (_: Request, res: Response) => res.render('login'))
+    app.get('/error', (_: Request, res: Response) => res.render('error'))
 } else {
-  app.get('/', protect, dashboard)
-  app.get('/auth/registration', authHandler('registration'))
-  app.get('/auth/login', authHandler('login'))
-  app.get('/error', errorHandler)
+    app.get('/', protect, dashboard)
+    app.get('/auth/registration', authHandler('registration'))
+    app.get('/auth/login', authHandler('login'))
+    app.get('/error', errorHandler)
 }
 
 app.get('/health', (_: Request, res: Response) => res.send('ok'))
 
 app.get('*', (_: Request, res: Response) => {
-  res.redirect('/')
+    res.redirect('/')
 })
 
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
-  console.error(err.stack)
-  res.status(500).render('error', {
-    message: JSON.stringify(err, null, 2),
-  })
+    console.error(err.stack)
+    res.status(500).render('error', {
+        message: JSON.stringify(err, null, 2),
+    })
 })
 
 const port = Number(process.env.PORT) || 3000
 app.listen(port, () => {
-  console.log(`Listening on http://0.0.0.0:${port}`)
+    console.log(`Listening on http://0.0.0.0:${port}`)
 })

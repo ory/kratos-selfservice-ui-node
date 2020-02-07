0b7bb3513ea85f97c6cdd673400ae3dffecb49fa
Patrik <zepatrik@users.noreply.github.com>
2020-02-07 11:28:08 +0100

    feat: Add profile management screen (#26)
    Closes #14 
    
    As the error screen is very primitive just changing the url to fetch errors from already closes #24, as I have done in this pr as well.

---
+++ src/profile.ts
@@ -0,0 +1,48 @@
+import { NextFunction, Request, Response } from 'express'
+import config from './config'
+import { AdminApi } from '@oryd/kratos-client'
+
+const adminApi = new AdminApi(config.kratos.admin)
+
+const profileHandler = (req: Request, res: Response, next: NextFunction) => {
+  const request = req.query.request
+
+  // The request is used to identify the login and registraion request and
+  // return data like the csrf_token and so on.
+  if (!request) {
+    console.log('No request found in URL, initializing auth flow.')
+    res.redirect(`${config.kratos.browser}/self-service/browser/flows/profile`)
+    return
+  }
+
+  adminApi
+    .getSelfServiceBrowserProfileManagementRequest(request)
+    .then(({ body, response }) => {
+      if (response.statusCode === 404) {
+        res.redirect(
+          `${config.kratos.browser}/self-service/browser/flows/profile`
+        )
+        return
+      } else if (response.statusCode != 200) {
+        return Promise.reject(body)
+      }
+
+      return Promise.resolve(body)
+    })
+    .then(request => {
+      if (request) {
+        res.render('profile', request.form)
+        return
+      }
+
+      return Promise.reject(
+        'Expected self service profile request to be defined.'
+      )
+    })
+    .catch(err => {
+      console.error(err)
+      next(err)
+    })
+}
+
+export default profileHandler

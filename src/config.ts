8b60ab8413e08ad670e15d205dc40c9a991e76f5
Kim Neunert <kneunert@gmail.com>
2020-07-10 15:21:17 +0200

    Update src/config.ts: Remove comment
    Co-authored-by: hackerman <3372410+aeneasr@users.noreply.github.com>

--- src/config.ts
+++ src/config.ts
@@ -8,7 +8,6 @@ export const logger = winston.createLogger({
   ),
   transports: [
     new winston.transports.Console(),
-    //new winston.transports.File({ filename: 'combined.log' })
   ]
 });
 

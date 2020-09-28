e2e6bdbae50991f296372197e5a5e318b7fcaba5
aeneasr <3372410+aeneasr@users.noreply.github.com>
2020-09-28 09:21:27 +0200

    style: format

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,19 +1,27 @@
-import {AxiosError} from "axios";
-import config from "./config";
-import {NextFunction,Response} from "express";
+import { AxiosError } from 'axios'
+import config from './config'
+import { NextFunction, Response } from 'express'
 
 export const isString = (x: any): x is string => typeof x === 'string'
 
-export const redirectOnSoftError = (res: Response, next: NextFunction, redirectTo: string) => (err: AxiosError) => {
-    if (!err.response) {
-        next(err)
-        return
-    }
-
-    if (err.response.status === 404 || err.response.status === 410 || err.response.status === 403) {
-        res.redirect(`${config.kratos.browser}${redirectTo}`)
-        return
-    }
-
+export const redirectOnSoftError = (
+  res: Response,
+  next: NextFunction,
+  redirectTo: string
+) => (err: AxiosError) => {
+  if (!err.response) {
     next(err)
+    return
+  }
+
+  if (
+    err.response.status === 404 ||
+    err.response.status === 410 ||
+    err.response.status === 403
+  ) {
+    res.redirect(`${config.kratos.browser}${redirectTo}`)
+    return
+  }
+
+  next(err)
 }

0e4b7656deab1704be0261e497bd959941d398c8
Andrew Franklin <andrewfranklin@Andrews-MacBook-Pro.local>
2021-07-14 11:50:30 -0700

    network tab restored

--- src/index.ts
+++ src/index.ts
@@ -91,9 +91,9 @@ if (process.env.NODE_ENV === 'stub') {
 app.get('/health', (_: Request, res: Response) => res.send('ok'));
 app.get('/debug', debug);
 
-app.get('*', (_: Request, res: Response) => {
-  res.redirect(config.baseUrl);
-});
+// app.get('*', (_: Request, res: Response) => {
+//   res.redirect(config.baseUrl);
+// });
 
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
   console.error(err.stack);

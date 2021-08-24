c6c943bc31134de82757f53acb79d4ad4e1efcaf
hackerman <3372410+aeneasr@users.noreply.github.com>
2021-08-24 14:05:59 +0200

    feat: forced MFA (#143)

--- src/middleware/simple.ts
+++ src/middleware/simple.ts
@@ -11,6 +11,7 @@ import { Configuration, V0alpha1Api } from '@ory/kratos-client'
 import config from '../config'
 import { NextFunction, Request, Response } from 'express'
 import urljoin from 'url-join'
+import { AxiosError } from 'axios'
 
 const kratos = new V0alpha1Api(
   new Configuration({ basePath: config.kratos.public })
@@ -25,7 +26,12 @@ export default (req: Request, res: Response, next: NextFunction) => {
       ;(req as Request & { user: any }).user = { session }
       next()
     })
-    .catch(() => {
+    .catch((err: AxiosError) => {
+      // 403 on toSession means that we need to request 2FA
+      if (err.response && err.response.status === 403) {
+        res.redirect(urljoin(config.baseUrl, '/auth/login?aal=aal2'))
+        return
+      }
       // If no session is found, redirect to login.
       res.redirect(urljoin(config.baseUrl, '/auth/login'))
     })

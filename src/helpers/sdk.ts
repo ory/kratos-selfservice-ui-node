e9b72616f0120feefd609b66a623a74dcda59958
aeneasr <3372410+aeneasr@users.noreply.github.com>
2021-09-11 13:55:35 +0300

    style: format

--- src/helpers/sdk.ts
+++ src/helpers/sdk.ts
@@ -6,24 +6,22 @@ export const isString = (x: any): x is string => typeof x === 'string'
 
 // Redirects to the specified URL if the error is an AxiosError with a 404, 410,
 // or 403 error code.
-export const redirectOnSoftError = (
-  res: Response,
-  next: NextFunction,
-  redirectTo: string
-) => (err: AxiosError) => {
-  if (!err.response) {
+export const redirectOnSoftError =
+  (res: Response, next: NextFunction, redirectTo: string) =>
+  (err: AxiosError) => {
+    if (!err.response) {
+      next(err)
+      return
+    }
+
+    if (
+      err.response.status === 404 ||
+      err.response.status === 410 ||
+      err.response.status === 403
+    ) {
+      res.redirect(`${config.kratos.browser}${redirectTo}`)
+      return
+    }
+
     next(err)
-    return
   }
-
-  if (
-    err.response.status === 404 ||
-    err.response.status === 410 ||
-    err.response.status === 403
-  ) {
-    res.redirect(`${config.kratos.browser}${redirectTo}`)
-    return
-  }
-
-  next(err)
-}

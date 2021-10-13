a709750edc4de0c6236dc1f1db83763e82637cfc
aeneasr <3372410+aeneasr@users.noreply.github.com>
2021-10-13 11:16:29 +0200

    fix: bump to newer v0alpha2 api

--- src/pkg/sdk/index.ts
+++ src/pkg/sdk/index.ts
@@ -1,18 +1,18 @@
 import { Configuration, V0alpha2Api } from '@ory/client';
-import { V0alpha2Api as OpenSourceV0alpha2Api } from '@ory/kratos-client';
+import { V0alpha2Api as OpenSourceV0alpha2Api, V0alpha2ApiInterface } from '@ory/kratos-client';
 
 export const apiBaseUrl = process.env.KRATOS_PUBLIC_URL || process.env.ORY_SDK_URL || 'https://playground.projects.oryapis.com';
 
 // Sets up the SDK using Ory Cloud
-let sdk: V0alpha2Api = new V0alpha2Api(
+let sdk: V0alpha2ApiInterface = new V0alpha2Api(
   new Configuration({ basePath: apiBaseUrl }),
-);
+) as unknown as V0alpha2ApiInterface;
 
 // Alternatively use the Ory Kratos SDK.
 if (process.env.KRATOS_PUBLIC_URL) {
   sdk = new OpenSourceV0alpha2Api(
     new Configuration({ basePath: apiBaseUrl }),
-  ) as unknown as V0alpha2Api;
+  );
 }
 
 export default sdk;

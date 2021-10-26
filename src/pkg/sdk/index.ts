f9f32592e4e06fd8e76095fddfb93854033c9f23
afreakk <laderud@hotmail.com>
2021-10-26 11:56:52 +0200

    feat: separate internal and browser url

--- src/pkg/sdk/index.ts
+++ src/pkg/sdk/index.ts
@@ -4,19 +4,23 @@ import {
   V0alpha2ApiInterface
 } from '@ory/kratos-client'
 
-export const apiBaseUrl =
+const apiBaseUrlInternal =
   process.env.KRATOS_PUBLIC_URL ||
   process.env.ORY_SDK_URL ||
   'https://playground.projects.oryapis.com'
 
+export const apiBaseUrl = process.env.KRATOS_BROWSER_URL || apiBaseUrlInternal
+
 // Sets up the SDK using Ory Cloud
 let sdk: V0alpha2ApiInterface = new V0alpha2Api(
-  new Configuration({ basePath: apiBaseUrl })
+  new Configuration({ basePath: apiBaseUrlInternal })
 ) as unknown as V0alpha2ApiInterface
 
 // Alternatively use the Ory Kratos SDK.
 if (process.env.KRATOS_PUBLIC_URL) {
-  sdk = new OpenSourceV0alpha2Api(new Configuration({ basePath: apiBaseUrl }))
+  sdk = new OpenSourceV0alpha2Api(
+    new Configuration({ basePath: apiBaseUrlInternal })
+  )
 }
 
 export default sdk

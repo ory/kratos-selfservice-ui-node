a891f711d3e868dd50fd2264064d010d66a27d1e
Grant Zvolsky <grant@zvolsky.org>
2022-10-05 11:16:45 +0000

    fix: update .prettierignore

--- src/routes/login.ts
+++ src/routes/login.ts
@@ -14,22 +14,28 @@ export const createLoginRoute: RouteCreator =
   (createHelpers) => async (req, res, next) => {
     res.locals.projectName = "Sign in"
 
-    const { flow, aal = "", refresh = "", return_to = "", login_challenge } = req.query
+    const {
+      flow,
+      aal = "",
+      refresh = "",
+      return_to = "",
+      login_challenge,
+    } = req.query
     const helpers = createHelpers(req)
     const { sdk, kratosBrowserUrl } = helpers
 
     const initFlowQuery = new URLSearchParams({
       aal: aal.toString(),
       refresh: refresh.toString(),
-      return_to: return_to.toString()
+      return_to: return_to.toString(),
     })
 
     if (isQuerySet(login_challenge)) {
-      logger.debug('login_challenge found in URL query: ', { query: req.query })
+      logger.debug("login_challenge found in URL query: ", { query: req.query })
       initFlowQuery.append("login_challenge", login_challenge)
     }
 
-    const initFlowUrl = getUrlForFlow(kratosBrowserUrl, 'login', initFlowQuery)
+    const initFlowUrl = getUrlForFlow(kratosBrowserUrl, "login", initFlowQuery)
 
     // The flow is used to identify the settings and registration flow and
     // return data like the csrf_token and so on.
@@ -57,15 +63,22 @@ export const createLoginRoute: RouteCreator =
         // Render the data using a view (e.g. Jade Template):
 
         const initRegistrationQuery = new URLSearchParams({
-            return_to: return_to.toString()
+          return_to: return_to.toString(),
         })
         if (flow.oauth2_login_request?.challenge) {
-          initRegistrationQuery.set("login_challenge", flow.oauth2_login_request.challenge)
+          initRegistrationQuery.set(
+            "login_challenge",
+            flow.oauth2_login_request.challenge,
+          )
         }
 
-        const initRegistrationUrl = getUrlForFlow(kratosBrowserUrl, 'registration', initRegistrationQuery)
+        const initRegistrationUrl = getUrlForFlow(
+          kratosBrowserUrl,
+          "registration",
+          initRegistrationQuery,
+        )
 
-        res.render('login', {
+        res.render("login", {
           ...flow,
           isAuthenticated: flow.refresh || flow.requested_aal === "aal2",
           signUpUrl: initRegistrationUrl,
--- src/routes/registration.ts
+++ src/routes/registration.ts
@@ -14,27 +14,38 @@ export const createRegistrationRoute: RouteCreator =
   (createHelpers) => (req, res, next) => {
     res.locals.projectName = "Create account"
 
-    const { flow, return_to = '', login_challenge } = req.query
+    const { flow, return_to = "", login_challenge } = req.query
     const helpers = createHelpers(req)
     const { sdk, kratosBrowserUrl } = helpers
 
-    const initFlowQuery = new URLSearchParams({ return_to: return_to.toString() })
+    const initFlowQuery = new URLSearchParams({
+      return_to: return_to.toString(),
+    })
 
     if (isQuerySet(login_challenge)) {
-      logger.debug('login_challenge found in URL query: ', { query: req.query })
+      logger.debug("login_challenge found in URL query: ", { query: req.query })
       initFlowQuery.append("login_challenge", login_challenge)
     } else {
-      logger.debug('no login_challenge found in URL query: ', { query: req.query })
+      logger.debug("no login_challenge found in URL query: ", {
+        query: req.query,
+      })
     }
 
-    const initFlowUrl = getUrlForFlow(kratosBrowserUrl, 'registration', initFlowQuery)
+    const initFlowUrl = getUrlForFlow(
+      kratosBrowserUrl,
+      "registration",
+      initFlowQuery,
+    )
 
     // The flow is used to identify the settings and registration flow and
     // return data like the csrf_token and so on.
     if (!isQuerySet(flow)) {
-      logger.debug("No flow ID found in URL query initializing registration flow", {
-        query: req.query,
-      })
+      logger.debug(
+        "No flow ID found in URL query initializing registration flow",
+        {
+          query: req.query,
+        },
+      )
       res.redirect(303, initFlowUrl)
       return
     }
@@ -43,13 +54,18 @@ export const createRegistrationRoute: RouteCreator =
       .getSelfServiceRegistrationFlow(flow, req.header("Cookie"))
       .then(({ data: flow }) => {
         // Render the data using a view (e.g. Jade Template):
-        const initLoginQuery = new URLSearchParams({ return_to: return_to.toString() })
+        const initLoginQuery = new URLSearchParams({
+          return_to: return_to.toString(),
+        })
         if (flow.oauth2_login_request?.challenge) {
-          initLoginQuery.set("login_challenge", flow.oauth2_login_request.challenge)
+          initLoginQuery.set(
+            "login_challenge",
+            flow.oauth2_login_request.challenge,
+          )
         }
         res.render("registration", {
           ...flow,
-          signInUrl: getUrlForFlow(kratosBrowserUrl, 'login', initLoginQuery),
+          signInUrl: getUrlForFlow(kratosBrowserUrl, "login", initLoginQuery),
         })
       })
       .catch(redirectOnSoftError(res, next, initFlowUrl))

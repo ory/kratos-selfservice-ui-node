fa2c9c1631895f60ac8c749f2f23c321b9e9c878
Alano Terblanche <18033717+Benehiko@users.noreply.github.com>
2023-06-19 11:42:42 +0200

    fix: users should return to previous screen on mfa required

--- src/pkg/middleware.ts
+++ src/pkg/middleware.ts
@@ -15,14 +15,14 @@ import { RouteOptionsCreator } from "./route"
  * @param apiBaseUrl
  */
 const maybeInitiate2FA =
-  (res: Response, apiBaseUrl: string) => (err: AxiosError) => {
+  (req: Request, res: Response, apiBaseUrl: string) => (err: AxiosError) => {
     // 403 on toSession means that we need to request 2FA
     if (err.response && err.response.status === 403) {
       res.redirect(
         getUrlForFlow(
           apiBaseUrl,
           "login",
-          new URLSearchParams({ aal: "aal2" }),
+          new URLSearchParams({ aal: "aal2", return_to: req.url.toString() }),
         ),
       )
       return true
@@ -37,11 +37,11 @@ const maybeInitiate2FA =
  */
 const addSessionToRequest =
   (req: Request) =>
-  ({ data: session }: { data: Session }) => {
-    // `whoami` returns the session or an error. We're changing the type here
-    // because express-session is not detected by TypeScript automatically.
-    req.session = session
-  }
+    ({ data: session }: { data: Session }) => {
+      // `whoami` returns the session or an error. We're changing the type here
+      // because express-session is not detected by TypeScript automatically.
+      req.session = session
+    }
 
 /**
  * This middleware requires that the HTTP request has a session.
@@ -53,29 +53,29 @@ const addSessionToRequest =
  */
 export const requireAuth =
   (createHelpers: RouteOptionsCreator) =>
-  (req: Request, res: Response, next: NextFunction) => {
-    const { frontend, apiBaseUrl } = createHelpers(req, res)
+    (req: Request, res: Response, next: NextFunction) => {
+      const { frontend, apiBaseUrl } = createHelpers(req, res)
 
-    // when accessing settings with a valid flow id
-    // we allow the settings page to trigger the
-    // login flow on session_aal2_required
-    if (req.url.includes("/settings") && req.query.flow) {
-      if (isUUID.test(req.query.flow.toString())) {
-        next()
-        return
-      }
-    }
-    frontend
-      .toSession({ cookie: req.header("cookie") })
-      .then(addSessionToRequest(req))
-      .then(() => next())
-      .catch((err: AxiosError) => {
-        if (!maybeInitiate2FA(res, apiBaseUrl)(err)) {
-          res.redirect(getUrlForFlow(apiBaseUrl, "login"))
+      // when accessing settings with a valid flow id
+      // we allow the settings page to trigger the
+      // login flow on session_aal2_required
+      if (req.url.includes("/settings") && req.query.flow) {
+        if (isUUID.test(req.query.flow.toString())) {
+          next()
           return
         }
-      })
-  }
+      }
+      frontend
+        .toSession({ cookie: req.header("cookie") })
+        .then(addSessionToRequest(req))
+        .then(() => next())
+        .catch((err: AxiosError) => {
+          if (!maybeInitiate2FA(req, res, apiBaseUrl)(err)) {
+            res.redirect(getUrlForFlow(apiBaseUrl, "login"))
+            return
+          }
+        })
+    }
 
 /**
  * Sets the session in the request. If no session is found,
@@ -87,14 +87,14 @@ export const requireAuth =
  */
 export const setSession =
   (createHelpers: RouteOptionsCreator) =>
-  (req: Request, res: Response, next: NextFunction) => {
-    const { frontend, apiBaseUrl } = createHelpers(req, res)
-    frontend
-      .toSession({ cookie: req.header("cookie") })
-      .then(addSessionToRequest(req))
-      .catch(maybeInitiate2FA(res, apiBaseUrl))
-      .then(() => next())
-  }
+    (req: Request, res: Response, next: NextFunction) => {
+      const { frontend, apiBaseUrl } = createHelpers(req, res)
+      frontend
+        .toSession({ cookie: req.header("cookie") })
+        .then(addSessionToRequest(req))
+        .catch(maybeInitiate2FA(req, res, apiBaseUrl))
+        .then(() => next())
+    }
 
 /**
  * This middleware requires that the HTTP request has no session.
@@ -104,14 +104,14 @@ export const setSession =
  */
 export const requireNoAuth =
   (createHelpers: RouteOptionsCreator) =>
-  (req: Request, res: Response, next: NextFunction) => {
-    const { frontend } = createHelpers(req, res)
-    frontend
-      .toSession({ cookie: req.header("cookie") })
-      .then(() => {
-        res.redirect("welcome")
-      })
-      .catch(() => {
-        next()
-      })
-  }
+    (req: Request, res: Response, next: NextFunction) => {
+      const { frontend } = createHelpers(req, res)
+      frontend
+        .toSession({ cookie: req.header("cookie") })
+        .then(() => {
+          res.redirect("welcome")
+        })
+        .catch(() => {
+          next()
+        })
+    }
